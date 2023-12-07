390fead1b3b08fc9782ac057799be411e9c10909
Vladik <v4054741@gmail.com>
2023-12-07 11:21:37 +0300

    Added entity,controller

---
+++ src/main/java/com/example/chat/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.example.chat.controller;
+
+
+import com.example.chat.models.GreetingEntity;
+import com.example.chat.models.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    private static final String HELLO = "/hello";
+    private static final String TP = "/topic/greetings";
+
+    @MessageMapping(HELLO)
+    @SendTo(TP)
+    public GreetingEntity greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000);
+        return new GreetingEntity("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
+

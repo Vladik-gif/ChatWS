112575351cdbdac129b628e20ab17fdcd2602776
Vladik <v4054741@gmail.com>
2023-12-15 20:39:09 +0300

    Update java 17 version

--- src/main/java/com/example/chat/models/GreetingEntity.java
+++ src/main/java/com/example/chat/models/GreetingEntity.java
@@ -1,17 +1,3 @@
 package com.example.chat.models;
 
-
-public class GreetingEntity {
-
-    private String context;
-
-    public GreetingEntity() {}
-
-    public GreetingEntity(String context) {
-        this.context = context;
-    }
-
-    public String getContext() {
-        return context;
-    }
-}
+public record GreetingEntity(String context) {}

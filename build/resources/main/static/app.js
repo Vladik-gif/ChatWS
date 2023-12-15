7b9625b6b3b1fb4bdf454d047c295b3840ac5321
Vladik <v4054741@gmail.com>
2023-12-15 19:27:39 +0300

    Add config project

---
+++ build/resources/main/static/app.js
@@ -0,0 +1,66 @@
+const URL = 'ws://localhost:8080/websocket';
+const URL_TOPIC = '/topic/greetings';
+const MESSAGE = 'message';
+const URL_APP = "/app/hello";
+
+
+const stompClient = new StompJs.Client({
+    brokerURL: URL
+});
+
+stompClient.onConnect = (frame) => {
+    setConnected(true);
+    console.log('Connected: ' + frame);
+    stompClient.subscribe(URL_TOPIC, (greeting) => {
+        showGreeting(JSON.parse(greeting.body).context);
+    });
+};
+
+stompClient.onWebSocketError = (error) => {
+    console.error('Error with websocket', error);
+};
+
+stompClient.onStompError = (frame) => {
+    console.error('Broker reported error: ' + frame.headers[MESSAGE]);
+    console.error('Additional details: ' + frame.body);
+};
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    stompClient.activate();
+}
+
+function disconnect() {
+    stompClient.deactivate();
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.publish({
+        destination: URL_APP,
+        body: JSON.stringify({'name': $("#name").val()})
+    });
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', (e) => e.preventDefault());
+    $( "#connect" ).click(() => connect());
+    $( "#disconnect" ).click(() => disconnect());
+    $( "#send" ).click(() => sendName());
+});
